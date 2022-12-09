e3873a038c82c0cfb9ee9f26e98c6f6f12c9a033
MKLNGLO <117443447+MKLNGLO@users.noreply.github.com>
2022-12-09 13:59:01 +0100

    Update D5.js

--- D5_v2/D5.js
+++ D5_v2/D5.js
@@ -131,3 +131,15 @@ while(i < numericArray.length){
     es. [f, b, e] --> [6, 2, 5]
 */
 const charactersArray = ["g", "n", "u", "z", "d"]
+
+function letterValue(str){
+  var anum={
+      a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, 
+      l: 12, m: 13, n: 14,o: 15, p: 16, q: 17, r: 18, s: 19, t: 20, 
+      u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
+  }
+  if(str.length== 1) return anum[str] || ' ';
+  return str.split('').map(letterValue);
+
+}
+console.log(letterValue);
